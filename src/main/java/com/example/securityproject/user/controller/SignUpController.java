6e77026c0e6618ec2ce8b24c8538e3142c51f82f
yong <dydydade@gmail.com>
2024-02-09 08:56:58 +0900

    [feat] #1 - signup controller 클래스 추가(로그인 페이지)

---
+++ src/main/java/com/example/securityproject/user/controller/SignUpController.java
@@ -0,0 +1,36 @@
+package com.example.securityproject.user.controller;
+
+import com.example.securityproject.user.dto.UserRegisterDto;
+import com.example.securityproject.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+@RequiredArgsConstructor
+public class SignUpController {
+
+    private final UserService userService;
+
+    /**
+     * @return 회원가입 페이지 리소스
+     */
+    @GetMapping
+    public String signup() {
+        return "signup";
+    }
+
+
+    @PostMapping
+    public String login(@ModelAttribute UserRegisterDto userRegisterDto) {
+        userService.signUp(
+                userRegisterDto.getUsername(),
+                userRegisterDto.getPassword()
+        );
+        return "redirect:login";
+    }
+}
