4ceb54235b7ee77951fe085f785af2bd02fea912
yong <dydydade@gmail.com>
2024-02-13 22:11:40 +0900

    [feat] #0 - PasswordEncoder Bean 등록(config 파일 생성)

---
+++ src/main/java/com/example/securityproject/config/PasswordEncoderConfig.java
@@ -0,0 +1,18 @@
+package com.example.securityproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * PasswordEncoder 빈 등록
+ */
+@Configuration
+public class PasswordEncoderConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
