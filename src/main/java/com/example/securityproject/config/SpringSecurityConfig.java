7a446ad9a227a80a3854a79e81ff127c6073a49f
yong <dydydade@gmail.com>
2024-02-14 23:25:49 +0900

    [feat] #0 - spring security config 파일 추가 (현재 파일을 추가했는데도 .permitAll("/signup") 으로 지정한 "/signup" 경로에 대해 인증 체크가 수행되는 문제가 있음. 수정 필요)

---
+++ src/main/java/com/example/securityproject/config/SpringSecurityConfig.java
@@ -0,0 +1,103 @@
+package com.example.securityproject.config;
+
+import com.example.securityproject.user.domain.User;
+import com.example.securityproject.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurityConfig {
+
+    private final UserService userService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // Basic 인증 비활성화
+                .httpBasic(httpBasic -> httpBasic.disable())
+                // CSRF 설정
+                .csrf(csrf -> csrf.disable())
+                // Remember-me 설정
+                .rememberMe(rememberMe -> rememberMe.tokenValiditySeconds(86400))
+                // 권한 설정
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/home", "/signup").permitAll()
+                        .requestMatchers("/note").hasRole("USER")
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/notice").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/notice").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                // 로그인 설정
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+    /**
+     * UserDetailsService 구현
+     *
+     * @return UserDetailsService
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            User user = userService.findByUsername(username);
+            if (user == null) {
+                throw new UsernameNotFoundException(username);
+            }
+            return user;
+        };
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
