85a3f089b99cd0dd7b0e5bc41a1abbb91d4bf061
yong <dydydade@gmail.com>
2024-02-17 18:25:47 +0900

    [fix] #0 - WebMvc 설정에 누락된 @Configuration 어노테이션 추가

--- src/main/java/com/example/securityproject/config/MvcConfig.java
+++ src/main/java/com/example/securityproject/config/MvcConfig.java
@@ -1,11 +1,13 @@
 package com.example.securityproject.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * WebMvc Config
  */
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
