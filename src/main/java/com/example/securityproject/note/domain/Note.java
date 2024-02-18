9239ad31c5e21dfe8e46374b051e5e8239667d4a
yong <dydydade@gmail.com>
2024-02-18 14:12:55 +0900

    [feat] #6 - Note domain 클래스 추가, 도메인 멤버변수 이름 변경사항(title → subject) 반영하여 view 수정

---
+++ src/main/java/com/example/securityproject/note/domain/Note.java
@@ -0,0 +1,86 @@
+package com.example.securityproject.note.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import com.example.securityproject.user.domain.User;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Note {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    // 제목
+    private String subject;
+
+    // 내용
+    @Lob
+    private String content;
+
+    // User 참조
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    // 생성일자
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    // 수정일자
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public Note(String subject, String content, User user) {
+        this.subject = subject;
+        this.content = content;
+        this.user = user;
+    }
+
+    public void updateContent(String subject, String content) {
+        this.subject = subject;
+        this.content = content;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
