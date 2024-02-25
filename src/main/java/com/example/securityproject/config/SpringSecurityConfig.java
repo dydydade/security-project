6ff4eca91951a8d29c20f53bf5d888a73a9b9b58
yong <dydydade@gmail.com>
2024-02-25 16:37:04 +0900

    [fix] #0 - 로그인 시 각종 resource 파일에 대해 spring security 설정이 적용되는 문제 해결(WebSecurityCustomizer 빈 등록하여 정적 resource는 spring security 대상에서 제외)

--- src/main/java/com/example/securityproject/config/SpringSecurityConfig.java
+++ src/main/java/com/example/securityproject/config/SpringSecurityConfig.java
@@ -3,11 +3,14 @@ package com.example.securityproject.config;
 import com.example.securityproject.user.domain.User;
 import com.example.securityproject.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
@@ -52,6 +55,12 @@ public class SpringSecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // 정적 리소스 spring security 대상에서 제외
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     /**
